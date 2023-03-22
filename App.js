05015bd1299a05afd24b56b38ac8c2e378633f7f
Raphael <Raphaelotto.RO@gmail.com>
2023-03-22 16:04:07 -0300

    UltimaAulaDoEduardo

--- App.js
+++ App.js
@@ -1,11 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, Text, View, StyleSheet, FlatList } from 'react-native';
+
+function Card(props) {
+  return (
+    <View style={styles.card}>
+      <Image style={{ resizeMode: 'stretch', height: 200}} source={{uri:props.livro.capa}} />
+      <Text> {props.livro.titulo} </Text>
+      <Text> R$ {props.livro.preco} </Text>
+    </View>
+  )
+}
 
 export default function App() {
+    const livros = [
+      {
+        capa: "https://cptstatic.s3.amazonaws.com/imagens/enviadas/materias/materia16043/caracteristicas-cavalos-saudaveis-artigos-cursos-cpt.jpg",
+        titulo: 'Livro 1',
+        preco: 25.90
+      },
+      {
+        capa: "https://images.booksense.com/images/460/994/9781788994460.jpg",
+        titulo: 'Livro 2',
+        preco: 125.90
+      },
+      {
+        capa: "https://images.booksense.com/images/460/994/9781788994460.jpg",
+        titulo: 'Livro 3',
+        preco: 225.90
+      },
+      {
+        capa: "https://images.booksense.com/images/460/994/9781788994460.jpg",
+        titulo: 'Livro 4',
+        preco: 325.90
+      },
+      {
+        capa: "https://images.booksense.com/images/460/994/9781788994460.jpg",
+        titulo: 'Livro 4',
+        preco: 325.90
+      },
+      {
+        capa: "https://images.booksense.com/images/460/994/9781788994460.jpg",
+        titulo: 'Livro 4',
+        preco: 325.90
+      },
+      {
+        capa: "https://images.booksense.com/images/460/994/9781788994460.jpg",
+        titulo: 'Livro 4',
+        preco: 325.90
+      },
+      {
+        capa: "https://images.booksense.com/images/460/994/9781788994460.jpg",
+        titulo: 'Livro 4',
+        preco: 325.90
+      },
+    ]
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{fontSize: 30, fontWeight: 'bold'}}>Livros </Text>
+      <View style={styles.conteudo}>
+        <FlatList 
+          data={livros}
+          renderItem={({item}) => <Card livro={item} />}
+        />
+      </View>
     </View>
   );
 }
@@ -13,8 +71,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 10,
+    marginTop: 40,
+    backgroundColor: '#ecf0f1',
   },
+  conteudo: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    backgroundColor: '#ccc',
+    paddingTop: 20,
+    alignItems: 'center',
+    width: '100%'
+  },
+  card: {
+    width: '40%',
+    height: 240,
+    backgroundColor: '#0d0',
+    borderRadius: 10,
+    margin: 15,
+  }
 });
